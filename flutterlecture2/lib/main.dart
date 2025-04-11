945460cd98d338bfe91b1c7b0537f2a32af56e7c
maaz <maazsaleem2265@gmail.com>
2025-04-11 06:16:17 -0700

    CodeUpdated

---
+++ flutterlecture2/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quran/quran.dart' as quran;
+import 'package:quran/translations/urdu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+      ),
+      home: Surahsindexscreen(),
+    );
+  }
+}
+
+class Surahsindexscreen extends StatefulWidget {
+  const Surahsindexscreen({super.key});
+
+  @override
+  State<Surahsindexscreen> createState() => _SurahsindexscreenState();
+}
+
+class _SurahsindexscreenState extends State<Surahsindexscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 114,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailSurah(index + 1)),
+              );
+            },
+            leading: CircleAvatar(child: Text("${index + 1}")),
+            title: Text(
+              quran.getSurahNameArabic(index + 1) +
+                  " | " +
+                  quran.getSurahName(index + 1),
+              style: GoogleFonts.amiriQuran(),
+            ),
+
+            subtitle: Text(quran.getSurahNameEnglish(index + 1)),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailSurah extends StatefulWidget {
+  var surahNum;
+  DetailSurah(this.surahNum, {super.key});
+
+  @override
+  State<DetailSurah> createState() => _DetailSurahState();
+}
+
+class _DetailSurahState extends State<DetailSurah> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: ListView.builder(
+            itemCount: quran.getVerseCount(widget.surahNum),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  quran.getVerse(
+                    widget.surahNum,
+                    index + 1,
+                    verseEndSymbol: true,
+                  ),
+                  textAlign: TextAlign.right,
+                  style: GoogleFonts.amiri(),
+                ),
+                subtitle: Text(
+                  quran.getVerseTranslation(
+                    widget.surahNum,
+                    index + 1,
+                    translation: quran.Translation.urdu),
+                  textDirection: TextDirection.rtl,
+                 ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
